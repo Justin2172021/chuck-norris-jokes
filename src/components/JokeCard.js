fdf023de243ded9ab2bbd7aef9a65fea4b8bf0e8
Justin2172021 <justin.busching@gmail.com>
2022-12-22 13:16:33 -0600

    Created components folder structure and a JokeCard component/CSS

---
+++ src/components/JokeCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./JokeCard.css";
+
+const JokeCard = () => {
+    return (
+        <>
+        <div className="joke-card">fdsdgfafg  dasfsda   asdsda   asdfsdafsda  sdaf fgasdgsdgasd dfgdfsgsdf dfgdf dsasgdfgdsdsgsd sdgsdagds adsfgasdgsda.</div>
+        </>
+    )
+}
+
+export default JokeCard;
